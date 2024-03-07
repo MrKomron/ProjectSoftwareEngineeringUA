7a51e20ff28415184099b09de491be075d6d0a16
Januel <januelblanco0910@gmail.com>
2024-03-07 14:08:56 +0100

    JOBBLUEPRINT

---
+++ src/Job.cpp
@@ -0,0 +1,38 @@
+#include "Job.h"
+
+// Constructor definition
+Job::Job(const std::string& name, int priority, int pages)
+        : jobName(name), jobPriority(priority), jobPages(pages), jobPrinted(false) {}
+
+// Setter and getter definitions
+void Job::setName(const std::string& name) {
+    jobName = name;
+}
+
+std::string Job::getName() const {
+    return jobName;
+}
+
+void Job::setPriority(int priority) {
+    jobPriority = priority;
+}
+
+int Job::getPriority() const {
+    return jobPriority;
+}
+
+void Job::setPages(int pages) {
+    jobPages = pages;
+}
+
+int Job::getPages() const {
+    return jobPages;
+}
+
+void Job::markPrinted() {
+    jobPrinted = true;
+}
+
+bool Job::isPrinted() const {
+    return jobPrinted;
+}
