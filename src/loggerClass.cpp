2d5e63f162951de85e0dfc8cbd1bf58f9b842626
MrKomron <vosidov.komron@gmail.com>
2024-06-22 15:04:06 +0200

    logger bestanden zijn toegevoegd

---
+++ src/loggerClass.cpp
@@ -0,0 +1,177 @@
+//
+// Created by admin on 4/30/2024.
+//
+
+#include "loggerClass.h"
+#include "XMLREADER/XMLReader.h"
+#include <iostream>
+
+using namespace std;
+
+void loggerLines::lines() {
+    cout << "==============================================================================================" << endl;
+    cout << endl;
+}
+void loggerLines::startSystem() {
+    cout << "===============================================     START     ===============================================" << endl;
+    cout << endl;
+}
+void loggerLines::startAutomated() {
+    cout << "============================================ Automated Processing Started ============================================" << endl;
+    cout << endl;
+}
+void loggerLines::lineAutomatedLoop(int loopCount) {
+    cout << "==================================     "<<loopCount<<"     ==================================" << endl;
+    cout << endl;
+}
+void loggerLines::startProcessedJob() {
+    cout << "========================   Processed Jobs   ========================" << endl;
+    cout << endl;
+}
+void loggerLines::lineAutomatedName(string deviceName) {
+    cout << "==========  "<< deviceName <<"  =========="<< endl;
+}
+void outputPrinter::accumulatedPagesDeviceAutomated(std::string deviceName, int accumulatedPages) {
+    cout << "Accumulated page of the device " << deviceName << " using automated process: " << accumulatedPages << endl;
+}
+void outputPrinter::startingPrint() {
+    cout << "Starting the printing process..." << endl;
+}
+void outputPrinter::startingScan() {
+    cout << "Starting the scanning process..." << endl;
+}
+void outputPrinter::allPagesPrinted() {
+    cout << endl;
+    cout << "All pages printed." << endl;
+}
+void outputPrinter::allPagesScanned() {
+    cout << endl;
+    cout << "All pages scanned." << endl;
+}
+void outputPrinter::printerBWFinished(string deviceName, int jobNumber,string jobUser, int pageCount, int totalCost){
+    cout << "Printer " << deviceName << " finished black-and-white job:"
+         << endl;
+    cout << "\t Job Number: " << jobNumber << endl;
+    cout << "\t Submitted by: " << jobUser << endl;
+    cout << "\t " << pageCount << " pages" << endl;
+    cout << "Total cost of this job: " << totalCost << endl;
+    cout << endl;
+}
+void outputPrinter::printerCOLFinished(string deviceName, int jobNumber, string jobUser, int pageCount, int totalCost) {
+    cout << "Printer " << deviceName << " finished color-printing job:"
+         << endl;
+    cout << "\t Job Number: " << jobNumber << endl;
+    cout << "\t Submitted by: " << jobUser << endl;
+    cout << "\t " << pageCount << " pages" << endl;
+    cout << "Total cost of this job: " <<totalCost << endl;
+    cout << endl;
+}
+void outputPrinter::scannerFinished(string deviceName, int jobNumber, string jobUser, int pageCount, int totalCost) {
+    cout << "Printer " << deviceName << " finished scanning job:" << endl;
+    cout << "\t Job Number: " << jobNumber << endl;
+    cout << "\t Submitted by: " << jobUser << endl;
+    cout << "\t " << pageCount << " pages" << endl;
+    cout << "Total cost of this job: " << totalCost << endl;
+    cout << endl;
+}
+void outputPrinter::printerInfoDevice(std::string deviceName, int emissions, std::string deviceType, int speed, int cost_per_page, int accumulatedPages, int totalEmissions, int totalEarnings) {
+    cout << "Device Name: " << deviceName << endl;
+    cout << "Emissions: " << emissions << endl;
+    cout << "Type: " << deviceType << endl;
+    cout << "Speed: " << speed << endl;
+    cout << "Cost per page: " << cost_per_page << endl;
+    cout << "Accumulated Pages: " << accumulatedPages << endl;
+    cout << "Total Emissions: " << totalEmissions << endl;
+    cout << "Total Earnings: " << totalEarnings << endl;
+}
+void outputPrinter::printerInfoJob(int jobNumber, int pageCount, string jobType, string userName, int totalCost, int totalEmissions) {
+    cout << "Job Number: " << jobNumber << endl;
+    cout << "Page Count: " << pageCount << endl;
+    cout << "Type: " << jobType << endl;
+    cout << "User Name: " << userName << endl;
+    cout << "Total Cost: " << totalCost << endl;
+    cout << "Total Emissions: " << totalEmissions << endl;
+    cout << endl;
+}
+void outputPrinter::numberEntriesDevice(int size) {
+    cout << "Number of Device entries: " << size << endl;
+}
+void outputPrinter::numberEntriesJob(int size) {
+    cout << "Number of Job entries: " << size << endl;
+}
+void outputPrinter::processedJobsHeader(string deviceName, int totalEmissionsDevice) {
+    cout << "Device Name: " << deviceName << endl;
+    cout << "Total Emissions of this device: " << totalEmissionsDevice << endl;
+    cout << "Jobs:" << endl;
+}
+void outputPrinter::processedJobsTail(int jobNumber, int pageCount, std::string jobType, std::string userName, int totalCost) {
+    cout << "Job Number: " << jobNumber << endl;
+    cout << "Page Count: " << pageCount << endl;
+    cout << "Job Type: " << jobType << endl;
+    cout << "Username: " << userName << endl;
+    cout << "Total cost of this job: " << totalCost << endl;
+    cout << endl;
+}
+void outputPrinter::debuggerLine(string symbol) {
+    string line;
+    for (int i = 0; i < 20; ++i) {
+        line += symbol;
+    }
+    cout << line << endl;
+}
+void outputPrinter::printSpecificDeviceInfo(DeviceInfo deviceInfo) {
+    cout << "Device Name: " << deviceInfo.deviceName << endl;
+    cout << "Emissions: " << deviceInfo.emissions << endl;
+    cout << "Type: " << deviceInfo.deviceType << endl;
+    cout << "Speed: " << deviceInfo.speed << endl;
+    cout << "Cost per page: " << deviceInfo.costpp << endl;
+    cout << "Accumulated Pages: " << deviceInfo.accumulatedPages << endl;
+    cout << "Total Emissions: " << deviceInfo.totalEmissions << endl;
+    cout << "Total Earnings: " << deviceInfo.totalEarnings << endl;
+    cout << endl;
+}
+
+void outputPrinter::printUnprocessedJobs(int jobNumber, int pageCount, string jobType, string userName, int totalCost) {
+    cout << "Job Number: " << jobNumber << endl;
+    cout << "Page Count: " << pageCount << endl;
+    cout << "Job Type: " << jobType << endl;
+    cout << "Username: " << userName << endl;
+    cout << "Total cost of this job: " << totalCost << endl;
+    cout << endl;
+}
+
+void outputPrinter::startUnprocessedJobs() {
+    cout << "========================   Unprocessed Jobs   ========================" << endl;
+    cout << endl;
+}
+
+void outputPrinter::updatedAccumulatedPagesAutomated(string deviceName, int accumulatedPages) {
+    cout << "Updated accumulated page of the device "<< deviceName <<" using automated process: " << accumulatedPages << endl;
+}
+
+void outputPrinter::printRemainingJobs(int jobNumber, int pageCount, string jobType, string userName) {
+    cout << "Job Number: " << jobNumber << ", Page Count: " << pageCount << ", Job Type: "<< jobType << ", Username: "<< userName << endl;
+}
+
+void outputPrinter::remainingAvailableJobs(string deviceName) {
+    cout << "Available unassigned jobs for device " << deviceName << ":" << endl;
+}
+
+void errorPrinter::noDeviceSystem() {
+    cerr << "No Device in the system." << endl;
+}
+void errorPrinter::noJobSystem() {
+    cerr << "No Job in the system." << endl;
+}
+void errorPrinter::noDeviceTypeExist(string jobType) {
+    cerr << "Error: No devices exist for the specified job type: " << jobType << endl;
+}
+void errorPrinter::noJobNumberFound(int jobNumber) {
+    cerr << "Job \"" << jobNumber << "\" not found." << endl;
+}
+void errorPrinter::jobTypeNotMatch(int jobNumber) {
+    cerr << "Job " << jobNumber << " type does not match the type of the Device." << endl;
+}
+void errorPrinter::noDeviceFoundName(string deviceName) {
+    cerr << "No device found with the name " <<deviceName<< " in the list of devices" << endl;
+}
---
+++ src/loggerClass.h
@@ -0,0 +1,57 @@
+//
+// Created by admin on 4/30/2024.
+//
+
+#ifndef PROJECTSOFTWAREENGINEERINGUA_LOGGERCLASS_H
+#define PROJECTSOFTWAREENGINEERINGUA_LOGGERCLASS_H
+#include <string>
+#include <iostream>
+#include "XMLREADER/XMLReader.h"
+using namespace std;
+
+class loggerLines {
+public:
+    static void lines();
+    static void startSystem();
+    static void startAutomated();
+    static void lineAutomatedLoop(int loopCount);
+    static void startProcessedJob();
+    static void lineAutomatedName(string deviceName);
+};
+class outputPrinter{
+public:
+    static void startingPrint();
+    static void startingScan();
+    static void allPagesPrinted();
+    static void allPagesScanned();
+    static void printerBWFinished(string deviceName, int jobNumber, string jobUser, int pageCount, int totalCost);
+    static void printerCOLFinished(string deviceName, int jobNumber, string jobUser, int pageCount, int totalCost);
+    static void scannerFinished(string deviceName, int jobNumber, string jobUser, int pageCount, int totalCost);
+    static void printerInfoDevice(string deviceName, int emissions, string deviceType, int speed, int cost_per_page, int accumulatedPages, int totalEmissions, int totalEarnings);
+    static void printerInfoJob(int jobNumber, int pageCount, string jobType, string userName, int totalCost, int totalEmissions);
+    static void numberEntriesDevice(int size);
+    static void numberEntriesJob(int size);
+    static void processedJobsHeader(string deviceName, int totalEmissionsDevice);
+    static void processedJobsTail(int jobNumber, int pageCount, string jobType, string userName, int totalCost);
+    static void accumulatedPagesDeviceAutomated(string deviceName, int accumulatedPages);
+    static void updatedAccumulatedPagesAutomated(string deviceName, int accumulatedPages);
+    static void debuggerLine(string symbol);
+    static void printSpecificDeviceInfo(DeviceInfo deviceInfo);
+    static void printSpecificJobInfo(JobInfo jobNumber);
+    static void remainingAvailableJobs(string deviceName);
+    static void printRemainingJobs(int jobNumber, int pageCount, string jobType, string userName);
+    static void printUnprocessedJobs(int jobNumber, int pageCount, string jobType, string userName, int totalCost);
+    static void startUnprocessedJobs();
+    static void emptyLine(){cout << endl;}
+};
+class errorPrinter{
+public:
+    static void noDeviceFoundName(string deviceName);
+    static void noJobNumberFound(int jobNumber);
+    static void jobTypeNotMatch(int jobNumber);
+    static void noDeviceTypeExist(string jobType);
+    static void noDeviceSystem();
+    static void noJobSystem();
+};
+
+#endif //PROJECTSOFTWAREENGINEERINGUA_LOGGERCLASS_H
